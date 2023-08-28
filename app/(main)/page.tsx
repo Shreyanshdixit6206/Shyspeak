4bd5fcc160f3a7c5d7d0370aed042b6dd1e8b32a
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-27 19:24:35 -0700

    changing bg color

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from '@/components/ui/mode-toggle'
 import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <main>
       <UserButton afterSignOutUrl="/"/>
+      <ModeToggle />
     </main>
   )
 }
