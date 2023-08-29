1388867007caeb71112eccb67aa38a2861b6cb81
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-29 00:01:08 -0700

    new channels layout

---
+++ app/(main)/servers/[serverId]/layout.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { currentProfile } from '@/lib/current-profile';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { db } from '@/lib/db';
+import { redirect } from 'next/navigation';
+import ServersSIdeBar from '@/components/server/SIdeBar';
+
+const MainLayout = async ({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: { serverId: string }
+}) => {
+  const SideBar = await ServersSIdeBar({ serverId: params.serverId });
+  const profile = await currentProfile();
+
+  if(!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+        id: params.serverId
+    }
+  });
+
+  if(!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className='h-full'>
+        <div className='hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0'>
+            {SideBar}
+        </div>
+        <main className='h-full md:pl-60'>
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default MainLayout;
