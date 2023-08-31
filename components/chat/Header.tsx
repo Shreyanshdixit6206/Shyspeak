60a434a812b659d87d9f585ccbcb511c24d68bf9
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-31 00:33:56 -0700

    updating Header component

--- components/chat/Header.tsx
+++ components/chat/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Hash } from 'lucide-react';
 import MobileToggle from '../MobileToggle';
+import UserAvatar from '../UserAvatar';
 
 interface ChatHeaderProps {
     serverId: string;
@@ -14,13 +15,16 @@ const ChatHeader = async ({ name, type, imageUrl, serverId }: ChatHeaderProps) =
 
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-        {mobileToggle}
-        {type === "channel" && (
-            <Hash className='h-5 w-5 text-zinc-500 dark:text-zinc-400 ml-2 mr-1' />
-        )}
-        <p className='font-semibold text-md text-black dark:text-white'>
-            {name}
-        </p>
+      {mobileToggle}
+      {type === "channel" && (
+          <Hash className='h-5 w-5 text-zinc-500 dark:text-zinc-400 ml-2 mr-1' />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className='h-8 w-8 md:h-8 md:w-8 mr-2' />
+      )}
+      <p className='font-semibold text-md text-black dark:text-white'>
+          {name}
+      </p>
     </div>
   )
 }
