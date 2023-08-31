7b6f1552e41d552f88045b6e390a17bce588b675
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-30 22:50:10 -0700

    adding redirection

--- components/server/Member.tsx
+++ components/server/Member.tsx
@@ -23,8 +23,12 @@ const ServerMember: React.FC<ServerMemberProps> = ({ member, server }) => {
   const router = useRouter();
   const icon = roleIconMap[member.role];
 
+  const onClick = () => {
+    router.push("/servers/" + server.id + "/conversations/" + member.id);
+  }
+
   return (
-    <button className={cn('group- px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-hover:bg-zinc-700/50 transition mb-1', params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
+    <button className={cn('group- px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-hover:bg-zinc-700/50 transition mb-1', params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700")} onClick={onClick}>
         <UserAvatar src={member.profile.imageUrl} className='h-8 w-8 md:h-8 md:w-0' />
         <p className={cn('font-semibold text-sm text-zinc-500 group-hover:texxt-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition', params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white")}>
             {member.profile.name}
