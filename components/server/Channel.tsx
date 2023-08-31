eb5fe571d118d94c59c77b1c3343c79dc033b21b
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-30 22:44:47 -0700

    minor changes

--- components/server/Channel.tsx
+++ components/server/Channel.tsx
@@ -6,7 +6,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import ActionTooltip from '../navigation/ActionTooltip';
-import { useModal } from '@/hooks/useModalStore';
+import { ModalType, useModal } from '@/hooks/useModalStore';
 
 interface ServerChannelProps {
     server: Server;
@@ -21,14 +21,23 @@ const iconMap = {
 }
 
 const ServerChannel: React.FC<ServerChannelProps> = ({ channel, server, role }) => {
+  const { onOpen } = useModal();
   const router = useRouter();
   const params = useParams();
-  const { onOpen } = useModal();
   const Icon = iconMap[channel.type];
+
+  const onClick = () => {
+    router.push("/servers/" + server.id + "/channels/" + channel.id);
+  };
+
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { server, channel });
+  };
 
   return (
     <button
-    //   onClick={onClick}
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -45,13 +54,13 @@ const ServerChannel: React.FC<ServerChannelProps> = ({ channel, server, role }) 
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
             <Edit
-              onClick={() => onOpen("editChannel", { channel, server })}
+              onClick={e => onAction(e, "editChannel")}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
           <ActionTooltip label="Delete">
             <Trash
-              onClick={() => onOpen("deleteChannel", { channel, server })}
+              onClick={e => onAction(e, "deleteChannel")}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
--- components/server/Member.tsx
+++ components/server/Member.tsx
@@ -26,7 +26,7 @@ const ServerMember: React.FC<ServerMemberProps> = ({ member, server }) => {
   return (
     <button className={cn('group- px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-hover:bg-zinc-700/50 transition mb-1', params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
         <UserAvatar src={member.profile.imageUrl} className='h-8 w-8 md:h-8 md:w-0' />
-        <p className={cn('font-semibold text-sm text-zinc-500 group-hover:texxt-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition', params?.channelId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white")}>
+        <p className={cn('font-semibold text-sm text-zinc-500 group-hover:texxt-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition', params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white")}>
             {member.profile.name}
         </p>
         {icon}
