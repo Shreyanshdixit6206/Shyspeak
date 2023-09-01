87a7651fa5bb6899645ca9f8273514b7f912eda9
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-31 18:04:19 -0700

    opening files

--- components/chat/Input.tsx
+++ components/chat/Input.tsx
@@ -10,6 +10,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { FormControl, FormField, FormItem } from '../ui/form';
 import { Input } from '../ui/input';
 import axios from 'axios';
+import { useModal } from '@/hooks/useModalStore';
 
 interface ChatInputProps {
   apiUrl: string;
@@ -28,6 +29,7 @@ const ChatInput: React.FC<ChatInputProps> = ({
   name,
   type,
 }) => {
+  const { onOpen } = useModal();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -61,7 +63,7 @@ const ChatInput: React.FC<ChatInputProps> = ({
             <FormItem>
               <FormControl>
                 <div className='relative p-4 pb-6'>
-                  <button className='absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center' type='button'>
+                  <button className='absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center' type='button' onClick={() => onOpen("messageFile", { apiUrl, query })}>
                     <Plus className='text-white dark:text-[#313338]' />
                   </button>
                   <Input placeholder={"Message " + (type === "channel" && "#") + name} disabled={isLoading} className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" {...field} />
