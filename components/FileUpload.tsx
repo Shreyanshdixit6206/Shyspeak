f3475282d05309b1a87e86d197ceab0f8d06ef11
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-31 18:04:08 -0700

    previewing pdf files

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -5,7 +5,7 @@ import { UploadDropzone } from '@/lib/uploadthing';
 import "@uploadthing/react/styles.css"
 import Image from 'next/image';
 import { Button } from './ui/button';
-import { X } from 'lucide-react';
+import { FileIcon, X } from 'lucide-react';
 
 interface FileUploadProps {
     endpoint: "serverImage" | "messageFile"
@@ -23,6 +23,23 @@ const FileUpload: React.FC<FileUploadProps> = ({ value, endpoint, onChange }) =>
                 <Button size="sm" onClick={() => onChange("")} className='bg-rose-500 hover:bg-rose-500/90 text-white p-0.5 aspect-square rounded-full absolute top-0 right-0 shadow-sm' type='button'>
                     <X className='h-3 w-3'/>
                 </Button>
+            </div>
+        );
+    }
+
+    if(value && fileType === "pdf") {
+        return(
+            <div className='relative flex items-center p-2 mt-2 rounded-md bg-baclground/10'>
+                <FileIcon className='h-10 w-10 fill-indigo-200 stroke-indigo-400' />
+                <a
+                    href={value}
+                    target='_blank'
+                    rel="noopener noreferrer"
+                    className='ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline'
+                >
+                    {value}
+                </a>
+                    <X className='h-4 w-4' />
             </div>
         );
     }
