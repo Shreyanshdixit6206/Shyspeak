6d2c66cdd3428d21ea216d529f894cd59f996527
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-08-31 18:53:37 -0700

    implementing the provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+
 import { ThemeProvider } from '@/components/providers/theme.provider'
 import { ModalProvider } from '@/components/providers/modal.provider'
 import { SocketProvider } from '@/components/providers/socket.provider'
+import { QueryProvider } from '@/components/providers/query.provider'
 import { cn } from '@/lib/utils'
+
+import './globals.css'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -25,8 +28,10 @@ export default function RootLayout({
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>
             <SocketProvider>
-              <ModalProvider />
-              {children}
+              <QueryProvider>
+                <ModalProvider />
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
