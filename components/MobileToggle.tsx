83ceaf24c90d6b0210da6a2fc3aa5ff5b56efd76
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-09-01 19:20:27 -0700

    other fix

--- components/MobileToggle.tsx
+++ components/MobileToggle.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Menu } from 'lucide-react'
+
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 import { Button } from './ui/button'
 import SideBar from './navigation/SideBar'
